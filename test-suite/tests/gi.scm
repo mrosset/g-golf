3d7e793742a9740c033aaad0fa8fba67931f4c13
David Pirotte <david@altosw.be>
2018-10-09 23:12:49 -0300

    Test-suite - Review, wip
    * test-suite/tests/gi.scm:  Reorganizing things so (a) nothing is used
      before it's been tested, then (b) follows the order in which modules
      are actually defines, which is also the order of the manual (both the
      original GObject Introspecton manual and G-Golf manual).
    
      Wip - it's much better as it is now, but this file and the test-suite
      as a all is largely incomplete still.

--- test-suite/tests/gi.scm
+++ test-suite/tests/gi.scm
@@ -1,7 +1,7 @@
 ;; -*- mode: scheme; coding: utf-8 -*-
 
 ;;;;
-;;;; Copyright (C) 2016
+;;;; Copyright (C) 2016 - 2018
 ;;;; Free Software Foundation, Inc.
 
 ;;;; This file is part of GNU G-Golf
@@ -32,11 +32,12 @@
   #:use-module (g-golf gi))
 
 
-(g-golf-ir-require "Clutter")
-(g-golf-ir-require "Gtk")
-
 (define-class <g-golf-test-gi> (<test-case>))
 
+
+;;;
+;;; Utils
+;;;
 
 (define-method (test-utils (self <g-golf-test-gi>))
   (assert-equal "clutter-actor"
@@ -45,36 +46,94 @@
 		(g-golf-gtype-name->class-name "ClutterActor")))
 
 
-(let* ((actor (g-golf-ir-find-by-name "Clutter" "Actor"))
-       (property (g-golf-oi-get-property actor 5))
-       (property-flags (g-golf-pi-get-flags property))
-       (align-info (g-golf-ir-find-by-name "Clutter" "ActorAlign")))
+;;;
+;;; Repository
+;;;
 
-  (define-method (test-repository (self <g-golf-test-gi>))
-    (assert-true actor)
-    (assert-true align-info)
-    (assert-exception  (g-golf-ir-require "ClutterBlue"))
-    (assert-equal #f
-		  (g-golf-ir-find-by-name "Gtk" "button"))
-    (assert-true (g-golf-ir-find-by-name "Gtk" "Button"))
-    (assert-true (g-golf-ir-get-version "Gtk")))
+(define-method (test-repository (self <g-golf-test-gi>))
+  (assert-true (g-golf-ir-require "Clutter"))
+  (assert-true (g-golf-ir-require "Gtk"))
+  (assert-true (g-golf-ir-get-typelib-path "Clutter"))
+  (assert-true (g-golf-ir-find-by-name "Clutter" "Actor"))
+  (assert-true (g-golf-ir-find-by-name "Clutter" "ActorAlign"))
+  (assert-exception (g-golf-ir-require "ClutterBlue"))
+  (assert-false (g-golf-ir-find-by-name "Gtk" "button"))
+  (assert-true (g-golf-ir-find-by-name "Gtk" "Button"))
+  (assert-true (g-golf-ir-get-version "Gtk")))
 
-  (define-method (test-registered-type-info (self <g-golf-test-gi>))
-    (assert-true (g-golf-rt-get-g-type align-info)))
 
-  (define-method (test-enum-info (self <g-golf-test-gi>))
+;;;
+;;; Base Info
+;;;
+
+(define-method (test-base-info (self <g-golf-test-gi>))
+  (assert-true (g-golf-bi-get-name (g-golf-ir-find-by-name "Clutter" "Actor"))))
+
+
+;;;
+;;; Callable Info
+;;;
+
+
+;;;
+;;; Function Info
+;;;
+
+
+;;;
+;;; Registered Type Info
+;;;
+
+(define-method (test-registered-type-info (self <g-golf-test-gi>))
+  (let ((align-info (g-golf-ir-find-by-name "Clutter" "ActorAlign")))
+    (assert-true (g-golf-rt-get-g-type align-info))))
+
+
+;;;
+;;; Enum Info
+;;;
+
+(define-method (test-enum-info (self <g-golf-test-gi>))
+  (let ((align-info (g-golf-ir-find-by-name "Clutter" "ActorAlign")))
     (assert-true (g-golf-ei-get-n-values align-info))
     (assert-true (g-golf-ei-get-value align-info 0))
-    (assert-true (g-golf-enum-import align-info)))
+    (assert-true (g-golf-enum-import align-info))))
 
-  (define-method (test-property (self <g-golf-test-gi>))
-    (assert-true property)
+
+;;;
+;;; Object Info
+;;;
+
+(define-method (test-object-info (self <g-golf-test-gi>))
+  (let ((actor (g-golf-ir-find-by-name "Clutter" "Actor")))
+    (g-golf-oi-get-property actor 5)))
+
+
+;;;
+;;; Arg Info
+;;;
+
+
+;;;
+;;; Property Info
+;;;
+
+(define-method (test-property-info (self <g-golf-test-gi>))
+  (let* ((actor (g-golf-ir-find-by-name "Clutter" "Actor"))
+         (property (g-golf-oi-get-property actor 5)))
+    (assert-true (g-golf-pi-get-flags property))
     (assert-equal "background-color"
 		  (g-golf-bi-get-name property))
-    (assert-numeric-= 3 0 property-flags)
+    (assert-numeric-= 3 0
+                      (g-golf-pi-get-flags property))
     (assert-equal '(readable writable)
 		  (g-golf-integer->gflags %g-golf-go-g-param-flags
-					  property-flags))))
+					  (g-golf-pi-get-flags property)))))
+
+
+;;;
+;;; Typelib
+;;;
 
 (define-method (test-type-lib (self <g-golf-test-gi>))
   (let ((filename (g-golf-ir-get-typelib-path "Clutter")))
