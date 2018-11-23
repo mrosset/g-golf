c8c086fa36d460a116c7ec1fc4a1c0a9630c56af
David Pirotte <david@altosw.be>
2018-11-23 01:24:21 -0200

    New interface - Working on (g-golf gobject params-vals)
    * g-golf/gobject/params-vals.scm (g-value->g-type):  New procedure, which
      gets and returns the scheme representation (a symbol) of the GType for
      a g-value.
    
    * doc/api-gobject-params-vals.texi:  Documenting the above.

--- g-golf/gobject/params-vals.scm
+++ g-golf/gobject/params-vals.scm
@@ -27,12 +27,14 @@
 
 
 (define-module (g-golf gobject params-vals)
+  #:use-module (ice-9 match)
   #:use-module (oop goops)
   #:use-module (system foreign)
   #:use-module (g-golf init)
   #:use-module (g-golf support enum)
   #:use-module (g-golf gobject enum-flags)
   #:use-module (g-golf gobject generic-values)
+  #:use-module (g-golf gobject type-info)
 
   #:duplicates (merge-generics
 		replace
@@ -40,8 +42,21 @@
 		warn
 		last)
 
-  #:export (g-value-get-float
+  #:export (g-value->g-type
+
+            g-value-get-float
             g-value-set-float))
+
+
+;;;
+;;; G-Golf Low Level API
+;;;
+
+(define (g-value->g-type g-value)
+  (match (parse-c-struct g-value
+                         (list unsigned-long double double))
+    ((g-type _ _)
+     (g-type->symbol g-type))))
 
 
 ;;;
