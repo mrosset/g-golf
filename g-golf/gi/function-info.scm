ea0b28fca089e43045a18f82acbbc8e9478bbfbe
David Pirotte <david@altosw.be>
2019-02-27 02:49:06 -0300

    Fixing (g-golf gi function-info)
    * g-golf/gi/function-info.scm: I forgot to export
      g-function-info-invoke, fixed.

--- g-golf/gi/function-info.scm
+++ g-golf/gi/function-info.scm
@@ -43,6 +43,7 @@
             g-function-info-get-property
             g-function-info-get-symbol
             g-function-info-get-vfunc
+            g-function-info-invoke
             %g-function-info-flags))
 
 
