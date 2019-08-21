63ff4cc3c80975144ef44e4ddeb52663a1e88347
David Pirotte <david@altosw.be>
2019-08-21 04:42:55 -0300

    Adding a g-instance/goops instance cache - Wip
      This is a wip because as it is, it will keep the goops instances that
      are cached 'alive' for ever, and this needs to be reviewed/enhanced so
      it becomes gc friendly. Also, it probably should be enhanced to become
      thread safe, but I need to thnk more abut this.
    
    * g-golf/hl-api/gtype.scm:
    
      %g-instance-cache-default-size, %g-instance-cache,
      g-instance-cache-ref, g-instance-cache-set!, g-instance-cache-show:
    
      The cache variables, ref, set! and show procedures, all new.
    
      make <gtype-class>: A new make method on the <gtype-class> metaclass
      is necessary, to avoid to allocate and initialize a new
      <gtype-instance> when the pointer to the g-instance is 'known' - is in
      the cache, in which case this methos just returns the goops instance
      associated with it.
    
      initialize <gtype-instance>): This method has been reviewed and
      greatly simplified, so it may rely on next-method to also initialize
      g-properties.

--- g-golf/hl-api/gtype.scm
+++ g-golf/hl-api/gtype.scm
@@ -49,7 +49,12 @@
 		last)
   
   #:export (<gtype-class>
-            <gtype-instance>))
+            <gtype-instance>
+            %g-instance-cache-default-size
+            %g-instance-cache
+            g-instance-cache-ref
+            g-instance-cache-set!
+            g-instance-cache-show))
 
 
 (g-export !info
@@ -116,31 +121,47 @@
   #:info #t
   #:metaclass <gtype-class>)
 
-(define-method (initialize (self <gtype-instance>) initargs)
-  ;; Init keywords for g-properties are cached at class creation time,
-  ;; see (g-golg hl-api gobject).  This method needs to call next-method,
-  ;; but without passing it any g-property init keyword, since these
-  ;; g-property slots redefine their slot-set! method, which calls
-  ;; g-object-set-property and that needs a g-inst which does not exist
-  ;; yet.
-  (let* ((c-name (class-name (class-of self)))
-         (g-props-init-kw (gi-cache-ref 'g-props-init-kw c-name)))
-    (receive (split-kw split-rest)
-        (split-keyword-args g-props-init-kw initargs)
-      (next-method self split-rest)
-      (gtype-instance-construct self initargs)
-      (gtype-instance-initialize-properties self split-kw))))
-
-(define (gtype-instance-construct self initargs)
+(define-method (make (class <gtype-class>) . initargs)
+  ;; If #:g-inst is passed, we first check if the g-inst is cached, in
+  ;; which case we just return the goops instance associated with it.
   (let ((g-inst (get-keyword #:g-inst initargs #f)))
-    (if g-inst
-        (set! (!g-inst self) g-inst)
-        (let* ((class (class-of self))
-               (gtype-id (!gtype-id class)))
-          (set! (!g-inst self)
-                (g-object-new gtype-id))))))
+    (or (and g-inst
+             (g-instance-cache-ref g-inst))
+        (next-method))))
 
-(define (gtype-instance-initialize-properties self g-props-init-kw)
+(define-method (initialize (self <gtype-instance>) initargs)
+  (let ((g-inst (or (get-keyword #:g-inst initargs #f)
+                    (g-instance-construct (class-of self)))))
+    (receive (split-kw split-rest)
+        (split-keyword-args '(#:g-inst) initargs)
+      (set! (!g-inst self) g-inst)
+      (next-method self split-rest)
+      (g-instance-cache-set! g-inst self))))
+
+(define (g-instance-construct class)
+  (g-object-new (!gtype-id class)))
+
+;; previous initialze core code, with its comment, which I may need in
+;; another context (the code).
+
+;; Init keywords for g-properties are cached at class creation time, see
+;; (g-golg hl-api gobject).  This method needs to call next-method, but
+;; without passing it any g-property init keyword, because g-property
+;; slots redefine their slot-set!  method to call g-object-set-property
+;; and that needs a g-inst which either does not exist yet (g-inst above
+;; is #f), or it does, but we don't know that goops will set the g-inst
+;; slot before any other, and therefore we cn't rely ibecause
+
+#;(let* ((c-name (class-name (class-of self)))
+       (g-props-init-kw (gi-cache-ref 'g-props-init-kw c-name)))
+  (receive (split-kw split-rest)
+      (split-keyword-args g-props-init-kw initargs)
+    (next-method self split-rest)
+    (g-instance-construct self initargs)
+    (g-instance-initialize-properties self split-kw)))
+
+;; we don't need the followig anymore, but let's keep it for now.
+#;(define (g-instance-initialize-properties self g-props-init-kw)
   (for-each (lambda (slot)
               (case (slot-definition-allocation slot)
                 ((#:g-property)
@@ -150,3 +171,26 @@
                    (and i-value
                         (slot-set! self s-name i-value))))))
       (class-direct-slots (class-of self))))
+
+
+;;;
+;;; g-instace-cache
+;;;
+
+(define %g-instance-cache-default-size 1013)
+
+(define %g-instance-cache
+  (make-hash-table %g-instance-cache-default-size))
+
+(define (g-instance-cache-ref g-inst)
+  (hashq-ref %g-instance-cache
+             (pointer-address g-inst)))
+
+(define (g-instance-cache-set! g-inst inst)
+  (hashq-set! %g-instance-cache
+              (pointer-address g-inst)
+              inst))
+
+(define (g-instance-cache-show)
+  (hash-for-each dimfi
+                 %g-instance-cache))
